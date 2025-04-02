b9db8ba8ba866626f779500284f39687627f36a5
AceCommander09 <138333283+AceCommander09@users.noreply.github.com>
2025-04-02 19:35:08 +1100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'AceCommander09.vercel.app',
+  description: 'AceCommander09 Official Website',
+  generator: 'acecommander09',
 }
 
 export default function RootLayout({
