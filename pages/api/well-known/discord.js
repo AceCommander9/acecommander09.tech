604091f17fb9c4bdf1dba0a29e47175814f8533d
AceCommander09 <138333283+AceCommander09@users.noreply.github.com>
2025-04-03 16:08:14 +1100

    Update discord.js

--- pages/api/well-known/discord.js
+++ pages/api/well-known/discord.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
     res.setHeader('Content-Type', 'text/plain');
-    res.status(200).send('Your custom content here');
+    res.status(200).send('dh=5593b7b11384447258d7649aa0edb772eb6375c4');
 }
 
